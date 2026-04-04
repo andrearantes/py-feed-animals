4fbb8350bbb56f1b5a1afc00cdf882c286c0af74
Andre Marcio <andre.marcio.ti@gmail.com>
2026-04-03 22:36:06 -0300

    verify task

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 class Animal:
 
-    def __init__(self, name: str, appetite: int, is_hungry: bool = True) -> None:
+    def __init__(self, name: str, appetite: int, is_hungry: bool = True)\
+            -> None:
         self.name = name
         self.appetite = appetite
         self.is_hungry = is_hungry
@@ -8,31 +9,27 @@ class Animal:
     def print_name(self) -> None:
         print(f"Hello, I'm {self.name}")
 
-    def feed(self) -> None:
+    def feed(self) -> int:
         if self.is_hungry:
             print(f"Eating {self.appetite} food points...")
-        self.is_hungry = False
-        return self.appetite
-        # se não estiver com fome: retorna 0
+            self.is_hungry = False
+            return self.appetite
         return 0
-
 
 class Cat(Animal):
 
-
+    def __init__(self, name, is_hungry: bool = True) -> int:
+        super().__init__(name, 3, is_hungry)
     def catch_mouse(self) -> None:
-        print(f"The hunt began!")
+         print("The hunt began!")
 
 class Dog(Animal):
 
-
-    def __init__(self, name, is_hungry: bool = True) -> None:
-        super().__init__(name, 7, is_hungry)
-
+    def __init__(self, name, is_hungry: bool = True) -> int:
+         super().__init__(name, 7, is_hungry)
 
     def bring_slippers(self) -> None:
-        print(f"The slippers delivered!")
+        print("The slippers delivered!")
 
-
-def feed_animals(animais: list) -> None:
-    return sum(animal.feed() for animal in animais)
+    def feed_animals(animals: list) -> int:
+        return sum(animal.feed for animal in animals)
